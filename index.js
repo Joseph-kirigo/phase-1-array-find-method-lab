8dca2d426301bd02d01f63d199320f157ae72fb1
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-24 09:21:23 +0300

    completed assognment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+let record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  function superbowlWin(item){
+    for(let team of item){
+        if(team.result==='W')
+        return team.year;
+    }
+  }
+  superbowlWin(record);
